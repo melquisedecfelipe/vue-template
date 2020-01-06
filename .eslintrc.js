e0716b71a8334d1bb02f944921b2b621b0d1d20e
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-01-06 09:59:42 -0200

    feat: add config to lint code and commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,25 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
+    es6: true,
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['airbnb-base', 'plugin:vue/base', 'plugin:prettier/recommended', 'prettier/vue'],
+  plugins: ['eslint-plugin-import-helpers'],
   rules: {
+    quotes: [2, 'single', { avoidEscape: true }],
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', '/components/', '/services/', '/store/', '/utils/', '/image/'],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
